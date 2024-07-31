7ebd10f33389afae0ba29c60541e729741327a86
Narayan singh <109469097+narayxnnn@users.noreply.github.com>
2024-07-31 05:51:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version of js
+
+// alert(3+3) we are using nodejs here not browser
+
+// most important thing is that code readability should be high
+
+let name = "narayan";
+let age = 22;
+let isadult = true;
+let state;
+let happy = null;
+
+
+//types of datatypes
+// number => 2 to the powr 53 tak
+// bigint = used for large numbers
+// string => " "
+// boolean => true/false
+//undefined
+// null = standalone value
+// symbol => unique
+
+
+//object
+
+console.log(typeof null); //typeof null is object
+console.log(typeof undefined);  //typeof undefined is undefined
