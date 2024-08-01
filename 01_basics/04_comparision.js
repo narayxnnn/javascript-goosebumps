9de69fa5863e3884b06024923f7e9bfbb6d62dbc
Narayan singh <109469097+narayxnnn@users.noreply.github.com>
2024-08-01 08:01:12 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// = < > == etc
+
+//on comparing diff data types
+
+console.log("2" > 1); //comparision operator convert number string into number
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);  //it consider null as zero
+
+// on comparing undefined we get false
+// === ye datatype bhi check karta hai
+// avoid above conversions
