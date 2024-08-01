63e09af80513c62ce41ea470af2c5d897227c7c8
Narayan singh <109469097+narayxnnn@users.noreply.github.com>
2024-08-01 10:17:43 +0000

    memory

---
+++ 01_basics/05_memories.js
@@ -0,0 +1,13 @@
+// generally we have two types of data types primitive and non promitive
+//primitive are those who gives values inside them like number, string, null etc majorly 7 typwas
+// non primityve are those who gives reference of the values inside them like array, object, function majorly
+
+
+
+//now
+// for storing datatypes we use stack and heap as the memory
+// stack stores primitive datatyp[es
+// and heap stores non primitive
+
+//inside stack jab hum values change karte hai tab every time ek new memory alot ho jati hai
+// but in heap values change karne par when we call changed name value vo usko priviouse wali memory se hi hi aata hai
