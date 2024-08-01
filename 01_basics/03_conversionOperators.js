e3474f0f35bcc555d871f6e14159f39a73087940
Narayan singh <109469097+narayxnnn@users.noreply.github.com>
2024-08-01 07:38:04 +0000

    operations

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -1,10 +1,10 @@
 let score = null;
 
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let convertedNumber = Number(score);
-console.log(convertedNumber);
-console.log(typeof(convertedNumber));
+//console.log(convertedNumber);
+//console.log(typeof(convertedNumber));
 
 // conclusion: if a string contain only no.s then it can be converted totally into number but 
 // if it has alphabet too then it will be NaN but its type will be number may its a fault in js
@@ -16,10 +16,24 @@ console.log(typeof(convertedNumber));
 // boolean conversion
 let isLogged = 1;
 let boolIsLogged = Boolean(isLogged);  // fuction for converting into boolean
-console.log(boolIsLogged);
+//console.log(boolIsLogged);
 
 // in into boolean conversion 1 & 0 into t/f resp...
 //on converting into blank string false and w fillled string true
 
 
 // same for converting into string we use String()
+
+
+
+
+//.                                  *******************************OPERATIONS***********************************
+
+
+// we so many basic operations here like + - * / ** %
+// we can add two strings too uk
+// butttt now the twist on adding one number and one string 
+console.log("1" + 2); // well it gave string
+console.log("1" + 2 + 2); // again string
+console.log(1 + 2 + "2"); // now it added first no.s 
+// so jo first hoga vo operation ko priority milegi
