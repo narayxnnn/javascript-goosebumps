f57c37713e6d0539e0176b55942fb75c10784a79
Narayan singh <109469097+narayxnnn@users.noreply.github.com>
2024-08-01 10:46:07 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,16 @@
+let name = "narayan";
+let age = 22;
+
+let intro = `hello i am ${name} and i am ${age} years old`;
+console.log(intro);
+
+// another way to write string
+
+let fullName = new String('narayansingh');
+// in this we can use so many functions like indexof, charat on this string because now its an object
+
+console.log(fullName[0]);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+
+// same .substring amd .slice we have to divide string into more parta
